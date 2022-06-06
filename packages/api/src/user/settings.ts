e23385e8e602e49b652f974a47873ef548da95a9
Rob De Feo <robdefeo@gmail.com>
2022-06-06 09:56:26 +0100

    feat: store address against profile (#123)
    * refactor: settings -> profile
    
    * chore: stop using variables in migration scripts
    
    * refactor: datastore and method names
    
    * chore: rename files to align with profile
    
    * feat: user addresses database functions
    
    * refactor: squirel -> loukoum
    
    * chore: remove some comments
    
    * feat: address list handlers
    
    * chore: prettier
    
    * fix: user correct API factory
    
    * chore: fix tests
    
    * chore: generate inbox protocs from spec project
    
    * chore: fix formatting
    
    GitOrigin-RevId: 524cc4aa87e6080e5041340ea825909070157fbe

--- packages/api/src/user/settings.ts
+++ packages/api/src/user/settings.ts
@@ -2,17 +2,17 @@ import { KeyRing } from '@mailchain/keyring';
 import axios from 'axios';
 import { initializeHeader } from '../auth/jwt';
 import { Configuration } from '../api/configuration';
-import { SettingsApiFactory } from '../api';
+import { UserApiFactory } from '../api';
 
 export async function getSettings(apiConfig: Configuration, kr?: KeyRing) {
 	if (kr !== undefined) initializeHeader(kr);
-	const settingsFactory = SettingsApiFactory(apiConfig, undefined, axios);
+	const settingsFactory = UserApiFactory(apiConfig, undefined, axios);
 	const { data } = await settingsFactory.getUserSettings();
 	return data.settings;
 }
 
 export async function setSetting(key: string, value: string, apiConfig: Configuration, kr?: KeyRing) {
 	if (kr !== undefined) initializeHeader(kr);
-	const settingsFactory = SettingsApiFactory(apiConfig, undefined, axios);
+	const settingsFactory = UserApiFactory(apiConfig, undefined, axios);
 	return settingsFactory.putUserSetting(key, { value });
 }
