ed1446733e8238344b88c72f510537e1de20e833
Rob De Feo <robdefeo@gmail.com>
2022-10-21 13:45:33 +0200

    refactor: remove algorand mneomic (#811)
    GitOrigin-RevId: 8bf6f3d5ea0c79dd646caeaab39390917abb6b28

--- packages/encoding/src/consts.ts
+++ packages/encoding/src/consts.ts
@@ -12,7 +12,6 @@ export const EncodingTypes = {
 	Base64: 'base64/plain',
 	Base62Url: 'base64/url',
 	Base58SubstrateAddress: 'base58/ss58-address',
-	MnemonicAlgorand: 'mnemonic/algorand',
 	Utf8: 'text/utf-8',
 } as const;
 
