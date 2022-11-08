005cc47c8208bc9d02b7109b8b018f1f40cd52c9
Rob De Feo <robdefeo@gmail.com>
2022-11-08 17:18:39 +0100

    chore: speed up unit tests
    This change:
    - creates a seperate jest config for libraries.
    - Library config runs in node and uses isolated modules
    - Run in band remove (not sure how the CI will deal with that) But much quicker locally and so far now problems
    
    GitOrigin-RevId: 132dad8b5e4ef7f8eb564598dfb4f60e105960de

--- packages/sdk/jest.config.cjs
+++ packages/sdk/jest.config.cjs
@@ -3,5 +3,4 @@ const { createConfig } = require('../../jest.config.cjs');
 
 module.exports = {
 	...createConfig(path.join(__dirname, '..', '..', 'packages')),
-	setupFilesAfterEnv: ['./jest.setup.js'],
 };
