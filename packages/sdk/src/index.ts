490ba2895538b7fa3b5080c2233b1a0ded156f0c
Rob De Feo <robdefeo@gmail.com>
2022-11-30 09:44:16 +0100

    fix: export-send-mail-result
    GitOrigin-RevId: 2284ec98d0c11b0f9f41c41bb84ef590ca5e1302

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,3 +1,4 @@
 export type { Configuration } from './mailchain';
 export { Mailchain } from './mailchain';
+export type { SendMailResult } from './mailchain';
 export type { Address, Attachment, SendMailParams } from './types';
