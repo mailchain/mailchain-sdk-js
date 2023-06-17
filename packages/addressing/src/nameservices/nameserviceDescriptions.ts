6d691b0d157aa1ad9ea7335323f1865a8ea771ef
Antonio <toteto@users.noreply.github.com>
2023-06-17 19:39:39 +0200

    fix: prevent spaceid nameservice autofilling for subdomains
    (#1798)
    
    GitOrigin-RevId: 20d55c610afd3f9fc40b19ef5cd4583e1c57e694

--- packages/addressing/src/nameservices/nameserviceDescriptions.ts
+++ packages/addressing/src/nameservices/nameserviceDescriptions.ts
@@ -1,6 +1,26 @@
 export type NameserviceDescription = {
+	/**
+	 * The name of the nameservice.
+	 *
+	 * Example: in `alice.eth@ens.mailchain.com`, the `ens` part is considered the name of the nameservice.
+	 */
 	readonly name: string;
+	/**
+	 * The list of domains that the nameservice supports.
+	 *
+	 * Example: in `alice.eth@ens.mailchain.com`, the `eth` part is considered the domain.
+	 */
 	readonly domains: string[];
+	/**
+	 * Whether the nameservice supports subdomains of the defined domains.
+	 *
+	 * Example: `alice.eth` is the regular domain, but having `billing.alice.eth` is considered subdomain.
+	 *
+	 * If the nameservice doesn't support subdomains, then `alice.eth` is the only valid domain.
+	 *
+	 * Defaults to `true`.
+	 */
+	readonly supportsSubdomains?: boolean;
 };
 
 export const NAMESERVICE_DESCRIPTIONS: NameserviceDescription[] = [
@@ -23,6 +43,7 @@ export const NAMESERVICE_DESCRIPTIONS: NameserviceDescription[] = [
 	{
 		name: 'spaceid',
 		domains: ['arb', 'bnb'],
+		supportsSubdomains: false,
 	},
 	{
 		name: 'tezosdomains',
